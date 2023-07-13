8a70306af82910b4ca20d1882ddf0bb21c90642e
Smithakunder <119489518+Smithakunder@users.noreply.github.com>
2023-07-13 06:31:45 +0000

    commit

--- priority.c
+++ priority.c
@@ -1,98 +1,103 @@
-#include<stdio.h>
-void printer(int processes[],int n,int burst_time[],int arrival_time[]) {
-    int WT[n],TAT[n],total_WT=0,total_TAT=0,completion_time[n],idle=0,RT[n];
-    for (int i = 0; i < n; i++) {
-        if(i==0) {
-            WT[0]=0;
-            completion_time[0]=burst_time[0]+arrival_time[0];
-            TAT[0]=completion_time[0]-arrival_time[0];
-            total_TAT=TAT[0];
-            RT[0]=0;
-            if(arrival_time[0]!=0) {
-                idle=arrival_time[0];
-            }
-            continue;
-        }
-        if(completion_time[i-1]<arrival_time[i]) {
-            completion_time[i]=arrival_time[i]+burst_time[i];
-            idle+=(arrival_time[i]-completion_time[i-1]);
-            RT[i]=0;
-        } else {
-            completion_time[i]=completion_time[i-1]+burst_time[i];
-            RT[i]=completion_time[i-1]-arrival_time[i];
-        }
-        TAT[i]=completion_time[i]-arrival_time[i];
-        total_TAT+=TAT[i];
-        WT[i]=TAT[i]-burst_time[i];
-        total_WT+=WT[i];
-    }
-    printf("Processes\tAT\tBT\tWT\tTAT\tCT\tRT\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t\t%d\t%d\t%d\t%d\t%d\t%d\n",processes[i],arrival_time[i],burst_time[i],WT[i],TAT[i],completion_time[i],RT[i]);
-    }
-    printf("AVG waiting time: %2f\n",(float)total_WT/n);
-    printf("AVG turnaround time: %2f\n",(float)total_TAT/n);
-    printf("Total Idle time: %d\n\n",idle);
-}
-void Sorter(int n,int processes[],int burst_time[],int arrival_time[])
+#include <stdio.h>
+// structure representing a structure
+struct priority_scheduling
 {
-    for (int i = 0; i < n; i++)//sorting in priority
+    // name of the process
+    char process_name;
+    // time required for execution
+    int burst_time;
+    // waiting time of a process
+    int waiting_time;
+    // total time of execution
+    int turn_around_time;
+    // priority of the process
+    int priority;
+};
+int main()
+{
+    // total number of processes
+    int number_of_process;
+    // total waiting and turnaround time
+    int total = 0;
+    // temporary structure for swapping
+    struct priority_scheduling temp_process;
+    // ASCII numbers are used to represent the name of the process
+    int ASCII_number = 65;
+    // swapping position
+    int position;
+    // average waiting time of the process
+    float average_waiting_time;
+    // average turnaround time of the process
+    float average_turnaround_time;
+    printf("Enter the total number of Processes: ");
+    // get the total number of the process as input
+    scanf("%d", &number_of_process);
+    // initializing the structure array
+    struct priority_scheduling process[number_of_process];
+    printf("\nPlease Enter the Burst Time and Priority of each process:\n");
+    // get burst time and priority of all process
+    for (int i = 0; i < number_of_process; i++)
     {
-        int first=i;
-        for (int j = i+1; j < n; j++) {
-            if (burst_time[j]<burst_time[first]) {
-                first=j;
-            }
+        // assign names consecutively using ASCII number
+        process[i].process_name = (char)ASCII_number;
+        printf("\nEnter the details of the process %c \n", process[i].process_name);
+        printf("Enter the burst time: ");
+        scanf("%d", &process[i].burst_time);
+        printf("Enter the priority: ");
+        scanf("%d", &process[i].priority);
+        // increment the ASCII number to get the next alphabet
+        ASCII_number++;
+    }
+    // swap process according to high priority
+    for (int i = 0; i < number_of_process; i++)
+    {
+        position = i;
+        for (int j = i + 1; j < number_of_process; j++)
+        {
+            // check if priority is higher for swapping
+            if (process[j].priority > process[position].priority)
+                position = j;
         }
-        int temp=processes[i];
-        processes[i]=processes[first];
-        processes[first]=temp;
-
-        temp=burst_time[i];
-        burst_time[i]=burst_time[first];
-        burst_time[first]=temp;
-
-        temp=arrival_time[i];
-        arrival_time[i]=arrival_time[first];
-        arrival_time[first]=temp;
+        // swapping of lower priority process with the higher priority process
+        temp_process = process[i];
+        process[i] = process[position];
+        process[position] = temp_process;
     }
-}
-void sjf(int processes[],int n,int burst_time[],int arrival_time[]) {
-    Sorter(n,processes,burst_time,arrival_time);
-    printf("SJF algorithm\n");
-    printer(processes,n,burst_time,arrival_time);
-}
-void main() {
-    /*ouput
-    Enter the number of processes: 4
-    Enter the arrival time for process 1:2
-    Enter the Burst time for processes 1:1
-    Enter the arrival time for process 2:1
-    Enter the Burst time for processes 2:5
-    Enter the arrival time for process 3:4
-    Enter the Burst time for processes 3:1
-    Enter the arrival time for process 4:0
-    Enter the Burst time for processes 4:6
-    SJF agorithm
-    Processes       AT      BT      WT      TAT     CT      RT
-    1               2       1       0       1       3       0
-    3               4       1       0       1       5       0
-    2               1       5       4       9       10      4
-    4               0       6       10      16      16      10
-    AVG waiting time: 3.500000
-    AVG turnaround time: 6.750000
-    Total Idle time: 3
-    */
-    int n=0;
-    printf("Enter the number of processes: ");
-    scanf("%d",&n);
-    int processes[n],burst_time[n],arrival_time[n];
-    for (int i = 0; i < n; i++) {
-        printf("Enter the arrival time for process %d:",i+1);
-        scanf("%d",&arrival_time[i]);
-        printf("Enter the Burst time for processes %d:",i+1);
-        scanf("%d",&burst_time[i]);
-        processes[i]=i+1;
+    // First process will not have to wait and hence has a waiting time of 0
+    process[0].waiting_time = 0;
+    for (int i = 1; i < number_of_process; i++)
+    {
+        process[i].waiting_time = 0;
+        for (int j = 0; j < i; j++)
+        {
+            // calculate waiting time
+            process[i].waiting_time += process[j].burst_time;
+        }
+        // calculate total waiting time
+        total += process[i].waiting_time;
     }
-    sjf(processes,n,burst_time,arrival_time);
+    // calculate average waiting time
+    average_waiting_time = (float)total / (float)number_of_process;
+    // assigning total as 0 for next calculations
+    total = 0;
+    printf("\n\nProcess_name \t Burst Time \t Waiting Time \t Turnaround Time\n");
+    printf("------------------------------------------------------------\n");
+    for (int i = 0; i < number_of_process; i++)
+    {
+        // calculating the turnaround time of the processes
+        process[i].turn_around_time = process[i].burst_time + process[i].waiting_time;
+        // calculating the total turnaround time.
+        total += process[i].turn_around_time;
+        // printing all the values
+        printf("\t %c \t\t %d \t\t %d \t\t %d", process[i].process_name, process[i].burst_time,
+               process[i].waiting_time, process[i].turn_around_time);
+        printf("\n-----------------------------------------------------------\n");
+    }
+    // calculating the average turn_around time
+    average_turnaround_time = (float)total / (float)number_of_process;
+    // average waiting time
+    printf("\n\n Average Waiting Time : %f", average_waiting_time);
+    // average turnaround time
+    printf("\n Average Turnaround Time: %f\n", average_turnaround_time);
+    return 0;
 }
